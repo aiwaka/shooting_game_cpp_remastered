aab70adcf03fc4cf87e2fa2377f64eceeee12953
aiwaka <Kw.I.KU.1414@gmail.com>
2022-09-20 12:52:00 +0900

    add constant and suit values

--- source/global_define.cpp
+++ source/global_define.cpp
@@ -1,6 +1,15 @@
 #include "global_define.hpp"
 
-const std::uint16_t GlobalValues::SCREEN_WIDTH = 1280;
-const std::uint16_t GlobalValues::SCREEN_HEIGHT = 960;
+const std::uint16_t GlobalValues::SCREEN_WIDTH = 640;
+const std::uint16_t GlobalValues::SCREEN_HEIGHT = 480;
 
 const float GlobalValues::PI = 3.141592654f;
+
+const int GlobalValues::OUT_WIDTH = 416;
+const int GlobalValues::OUT_HEIGHT = 480;
+const int GlobalValues::IN_WIDTH = 384;
+const int GlobalValues::IN_HEIGHT = 448;
+const int GlobalValues::CENTER_X = (OUT_WIDTH / 2);
+const int GlobalValues::CENTER_Y = (OUT_HEIGHT / 2);
+const int GlobalValues::IN_TOP_LEFT_X = ((OUT_WIDTH - IN_WIDTH) / 2);
+const int GlobalValues::IN_TOP_LEFT_Y = ((OUT_HEIGHT - IN_HEIGHT) / 2);
--- source/global_define.hpp
+++ source/global_define.hpp
@@ -8,6 +8,18 @@ public:
 
     const static float PI;
 
+    // OUTは外枠, INは内枠
+    const static int OUT_WIDTH;
+    const static int OUT_HEIGHT;
+    const static int IN_WIDTH;
+    const static int IN_HEIGHT;
+    // 移動可能範囲の中心座標
+    const static int CENTER_X;
+    const static int CENTER_Y;
+    // 内枠の左上の座標
+    const static int IN_TOP_LEFT_X;
+    const static int IN_TOP_LEFT_Y;
+
     enum Stage {
         Stage1,
     };
